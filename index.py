71a5a8cc0aa1e6f31518f2ba393663397f2d05b0
Carlo Naling <nalingcarlo@gmail.com>
2024-03-04 20:27:19 +0800

    Version 1

---
+++ index.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar  4 19:38:32 2024
+
+@author: Carlo
+"""
+
+name = "Mark";
+num = 1
+
+print("-------------------");
+print("Hello GitHub World!");
+print();
+print("My name is", name);
+print();
+print("This is Version", num);
+print("-------------------");
